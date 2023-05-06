36bdf33154aeda149644206ffaaf3e6dd7e8f6cc
hotredsam <hotredsam@gmail.com>
2023-05-06 11:36:53 -0700

    added some actual functionality and got openai working

--- index.js
+++ index.js
@@ -1,101 +1,67 @@
-const questionEl = document.getElementById('question');
-const optionsEl = document.getElementById('options');
-const resultEl = document.getElementById('result');
-const mapEl = document.getElementById('map');
 
-const geonamesUsername = 'hotredsam';
-const openaiApiKey = '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';
+let latitude = "";
+let longitude = "";
 
-// Helper function to fetch GPT-4 response
-async function fetchGpt4Response(prompt) {
-    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${openaiApiKey}`,
-        },
-        body: JSON.stringify({
-            prompt,
-            max_tokens: 50,
-            n: 1,
-            stop: null,
-            temperature: 0.7,
-        }),
+// const configuration = new Configuration({
+//     organization: "org-TqEpdlmkfsBxbmm8zCNbwlWm",
+//     apiKey: "sk-",
+// });
+// const openai = new OpenAIApi(configuration);
+
+
+function updateCoordinates() {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            // Request the current location
+            navigator.geolocation.getCurrentPosition(
+                // Success callback
+                function (position) {
+                    // Get latitude and longitude
+                    const latitude = position.coords.latitude;
+                    const longitude = position.coords.longitude;
+                    resolve({ latitude, longitude });
+                },
+                // Error callback
+                function (error) {
+                    console.error("Error getting location:", error.message);
+                    reject(error);
+                }
+            );
+        } else {
+            console.error("Geolocation is not supported by your browser.");
+            reject(new Error("Geolocation is not supported by your browser."));
+        }
     });
-
-    const data = await response.json();
-    return data.choices[0].text.trim();
 }
 
-// Get user's location
-navigator.geolocation.getCurrentPosition(async (position) => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+async function getCoordinates() {
+    try {
+        const result = await updateCoordinates();
+        latitude = result.latitude.toString();
+        longitude = result.longitude.toString();
+        console.log(latitude + " is my latitude" + " and " + longitude + " is my longitude");
+        const promptCities = "can you give me the 100 closest cities to " + latitude + " is my latitude and " + longitude + " is my longitude. Give me no words except for the city names seperated by commas starting with the closest cities to " + latitude + " is my latitude and " + longitude + " is my longitude"
 
-    console.log(lon);
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + "sk-", // Replace with your actual OpenAI API key
+            },
+            body: JSON.stringify({
+                model: "gpt-4",
+                messages: [{ "role": "user", "content": promptCities }],
+                max_tokens: 500,
+                temperature: 0,
+            }),
+        });
 
-    // Fetch 100 closest cities
-    const citiesUrl = `https://secure.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lon}&cities=cities1000&maxRows=100&radius=300username=${geonamesUsername}`;
-    const citiesResponse = await fetch(citiesUrl);
-    const citiesData = await citiesResponse.json();
-
-    console.log(citiesResponse);
-
-    const cities = citiesData.geonames;
-    if (!cities || cities.length === 0) {
-        console.error('No cities found');
-        return;
+        const data = await response.json();
+        console.log(data["choices"][0]["message"]["content"]);
+    } catch (error) {
+        console.error("Error getting coordinates:", error.message);
     }
+}
 
-    const randomCity = cities[Math.floor(Math.random() * cities.length)];
 
-    // Generate GPT-4 question
-    const questionPrompt = `Generate a fictional scenario trivia question involving the city of ${randomCity.name}, ${randomCity.countryCode}, and its distance from the user's location at latitude ${lat} and longitude ${lon}.`;
-    const questionText = await fetchGpt4Response(questionPrompt);
-
-    questionEl.textContent = questionText;
-
-    // Generate GPT-4 multiple choice options
-    const optionsPrompt = `Generate 4 multiple choice options for the trivia question: "${questionText}"`;
-    const optionsText = await fetchGpt4Response(optionsPrompt);
-
-    const options = optionsText.split('\n').filter(option => option.trim());
-    options.forEach((option, index) => {
-        const button = document.createElement('button');
-        button.textContent = option;
-        button.onclick = async () => {
-            // Check the answer using GPT-4
-            const answerPrompt = `Is the answer "${option}" correct for the trivia question: "${questionText}"?`;
-            const answerResult = await fetchGpt4Response(answerPrompt);
-            resultEl.textContent = answerResult;
-
-            // Display map with an overlay
-            const map = L.map('map').setView([lat, lon], 8);
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            }).addTo(map);
-
-            // Add markers for user's location and the random city
-            L.marker([lat, lon]).addTo(map).bindPopup('Your location');
-            L.marker([randomCity.lat, randomCity.lng]).addTo(map).bindPopup(`${randomCity.name}, ${randomCity.countryCode}`);
-
-            // Add polyline between user's location and the random city
-            const polyline = L.polyline([
-                [lat, lon],
-                [randomCity.lat, randomCity.lng],
-            ], { color: 'red' }).addTo(map);
-            // Add distance overlay
-            const distance = map.distance([lat, lon], [randomCity.lat, randomCity.lng]);
-            const distanceOverlay = L.control({ position: 'bottomleft' });
-            distanceOverlay.onAdd = () => {
-                const div = L.DomUtil.create('div', 'distance-overlay');
-                div.innerHTML = `<strong>Distance:</strong> ${(distance / 1000).toFixed(1)} km`;
-                return div;
-            };
-            distanceOverlay.addTo(map);
-        };
-
-        optionsEl.appendChild(button);
-
-    });
-});
+getCoordinates();
